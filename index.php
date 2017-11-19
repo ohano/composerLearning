c9c04493d01e9ee8db4b531227181ce2b4a922c3
hano <499928027@qq.com>
2017-11-20 00:00:47 +0800

    update

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
+
+
+$log = new Monolog\Logger('name');
+var_dump($log);exit();
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+
+$log->addWarning('Foo');
